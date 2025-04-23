766f004d6d7b9d308a3d0597787a5d897486fdd1
prapticnayak@gmail.com <prapticnayak@gmail.com>
2025-04-23 14:51:40 +0530

    save to main

---
+++ bca216/Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        
+        System.out.println("Addition: "+(a+b));
+        System.out.println("Subtraction: "+(b-a));
+    }
+}
