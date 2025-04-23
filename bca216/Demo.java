53091251bc42366659a71386e27abde3cd29cc54
PraptiNayak25 <prapticnayak@gmail.com>
2025-04-23 17:09:17 +0530

    Update Demo.java

--- bca216/Demo.java
+++ bca216/Demo.java
@@ -2,7 +2,7 @@ public class Demo{
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        
+       System.out.println("Addition: "+(a+b)); 
         System.out.println("Multiplication: "+(a*b));
         System.out.println("Subtraction: "+(b-a));
     }
